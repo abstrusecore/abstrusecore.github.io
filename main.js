c188ccf7bfdb71c3410e5152f487e07eb9a5bad7
AbstruseCore <harleysteven101@gmail.com>
2020-08-13 05:22:55 -0700

    Updated layout

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 $(document).ready(() =>{
   $('.projects').on('click', () => {
     if (parseInt(window.innerWidth) >= 640  || parseInt(window.clientWidth) < 640 ) {
-      document.getElementById("me").src = "./Resume.png";
+      document.getElementById("me").src = "./images/Resume.png";
     } else {
-      document.getElementById("me").src = "./longResume.png";
+      document.getElementById("me").src = "./images/longResume.png";
     }
     $('.contact').fadeToggle(700);
   });
-
   /* $('.notification').on('click', event => {
     $(event.currentTarget).toggleClass('active');
   })  */
